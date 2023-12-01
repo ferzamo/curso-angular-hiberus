35879e7f17893cae085b465e4fcc46fc9f99b988
Fernando Zamora Diez <fzamora@hiberus.com>
2023-12-01 15:06:44 +0100

    firebase integration

--- projects/practica/src/environments/environment.ts
+++ projects/practica/src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    projectId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    storageBucket: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    messagingSenderId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    apiKey: 'AIzaSyB2iJirzgl8EExX4h8j7jg-3JwYd-p6QL4',
+    authDomain: 'practica-angular-b3845.firebaseapp.com',
+    projectId: 'practica-angular-b3845',
+    storageBucket: 'practica-angular-b3845.appspot.com',
+    messagingSenderId: '64723127767',
+    appId: '1:64723127767:web:af2904f64925d9f5818f92',
+    measurementId: 'G-XBKV1X7E00',
   },
 };
